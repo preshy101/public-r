490dd7ddb351b913ebcee165ccea2350dbf1a9dc
Precious Onwukwe <38045337+preshy101@users.noreply.github.com>
2024-01-13 15:38:40 +0100

    softDelete bug fixes

--- app/Models/cms.php
+++ app/Models/cms.php
@@ -3,15 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\softDeletes; 
+use Illuminate\Database\Eloquent\SoftDeletes; 
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class cms extends Model
 {
     use HasFactory;
-
-    use softDeletes;
+    use SoftDeletes;
     protected $guarded = [];
 
     public function imageVideo(): HasMany{
