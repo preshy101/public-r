cb11bd649c561d0cea8f7dd0a2fc9fabb51d0217
Precious Onwukwe <presh449@gmail.com>
2024-06-10 18:20:54 +0100

    Gray link

--- resources/views/livewire/includes/header.blade.php
+++ resources/views/livewire/includes/header.blade.php
@@ -542,7 +542,7 @@
 						    {{(Route::is('contactus'))?"active":''}} "><a wire:navigate href="/contact-us" style="text-transform: unset">Contact</a></li> --}}
 							 <ul class="dropdown-menu">
 								<li><a wire:navigate href="/news/posts"style="text-transform: unset">NIPR News </a></li>
-								<li><a wire:navigate href="/news/newslink"style="text-transform: unset;">Newslinks </a></li>
+								<li><a  href="/news/newslink"style="text-transform: unset; color:gray">Newslinks </a></li>
 								<li>
 									<a wire:navigate href="#" style="text-transform: unset;  color:gray">Press Releases</a>
 								</li>
