f2093cdbabf970cc93384a89b3e59fdc0727c4fc
NIPR-ICT <ICT@niprng.org.ng>
2024-07-15 11:14:47 +0100

    twak-to script

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -143,6 +143,19 @@
         </div> <!-- .cntact-info -->
 
     </footer> <!-- .site-footer -->
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6694f2c532dca6db2caf7736/1i2qt8lfh';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
     @livewireScripts
 
     @include('livewire.includes.footer')
