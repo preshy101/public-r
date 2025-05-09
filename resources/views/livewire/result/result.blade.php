eb970a7d14743dfbdb75875777fb2f2f109d9da4
Precious Onwukwe <preshcodes@gmail.com>
2025-05-09 12:32:25 +0100

    result year 2025

--- resources/views/livewire/result/result.blade.php
+++ resources/views/livewire/result/result.blade.php
@@ -69,6 +69,7 @@
                 <label for="file">Year</label>
                 <select name="year" id=""  class="form-control">
                     <option disabled selected>Select Year</option>
+                    <option value="2025">2025</option>
                     <option value="2024">2024</option>
                     <option value="2023">2023</option>
                     <option value="2022">2022</option>
